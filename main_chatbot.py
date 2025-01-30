729ea4dbfb963b4ad413699eefbe05171b25c4c6
Jas Matharu <mjas0401@gmail.com>
2025-01-30 12:42:49 +0530

    file created

---
+++ main_chatbot.py
@@ -0,0 +1,61 @@
+import os, getpass
+from langchain_openai import ChatOpenAI
+from pprint import pprint
+from langchain_core.messages import SystemMessage, HumanMessage, RemoveMessage
+from IPython.display import Image, display
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph, START, END
+
+llm = ChatOpenAI(model_name="gpt-4", temperature=0.7)
+
+def set_env(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"Enter {var}: ")
+
+set_env("OPENAI_API_KEY")
+set_env("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+
+# messages = [AIMessage()]
+
+def chat_model_node(state: MessagesState):
+    return {"messages": llm.invoke(state["messages"])}
+
+class State(MessagesState):
+    summary: str
+def call_model(state: State):
+    
+    # Get summary if it exists
+    summary = state.get("summary", "")
+
+    # If there is summary, then we add it
+    if summary:
+        
+        # Add summary to system message
+        system_message = f"Summary of conversation earlier: {summary}"
+
+        # Append summary to any newer messages
+        messages = [SystemMessage(content=system_message)] + state["messages"]
+    
+    else:
+        messages = state["messages"]
+    
+    response = llm.invoke(messages)
+    return {"messages": response}
+
+def summarize_conversation(state: State):
+    
+    # First, we get any existing summary
+    summary = state.get("summary", "")
+
+    # Create our summarization prompt 
+    if summary:
+        
+        # A summary already exists
+        summary_message = (
+            f"This is summary of the conversation to date: {summary}\n\n"
+            "Extend the summary by taking into account the new messages above:"
+        )
+        
+    else:
+        summary_message = "Create a summary of the conversation above:"
